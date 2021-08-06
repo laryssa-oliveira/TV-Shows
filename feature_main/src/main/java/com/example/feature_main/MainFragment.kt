de3eb2637c9ccf473dab8deb3dd33c8bee31ddba
Laryssa Oliveira <laryssa_olvgyn@hotmail.com>
2021-08-06 19:19:17 -0300

    Initial commit

---
+++ feature_main/src/main/java/com/example/feature_main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.feature_main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.feature_main.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
