a9288daabbaaef34b8fab3c2f4b0118ca6a3ca28
Laryssa Oliveira <laryssa_olvgyn@hotmail.com>
2021-08-18 16:00:09 -0300

    Fixing project

--- di/src/main/java/com/example/di/DomainModule.kt
+++ di/src/main/java/com/example/di/DomainModule.kt
@@ -1,10 +1,12 @@
 package com.example.di
 
 import com.example.domain.core.ThreadContextProvider
+import com.example.domain.usecases.MainUseCase
 import kotlinx.coroutines.CoroutineScope
 import org.koin.dsl.module
 
 val domainModule = module {
     single { ThreadContextProvider() }
+    factory { (scope: CoroutineScope) -> MainUseCase(scope, get()) }
 
 }
