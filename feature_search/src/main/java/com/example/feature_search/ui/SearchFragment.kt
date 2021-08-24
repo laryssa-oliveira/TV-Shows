9b783a02c61ecbeb48d981c22c0c455a3b8c7d65
Laryssa Oliveira <laryssa_olvgyn@hotmail.com>
2021-08-24 10:39:33 -0300

    Search updated

--- feature_search/src/main/java/com/example/feature_search/ui/SearchFragment.kt
+++ feature_search/src/main/java/com/example/feature_search/ui/SearchFragment.kt
@@ -1,15 +1,21 @@
 package com.example.feature_search.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import com.example.base_feature.ShowAdapter
 import com.example.base_feature.ViewState
+import com.example.base_feature.mappers.ShowMapper.toShowModel
 import com.example.base_feature.model.ShowPresentation
 import com.example.base_feature.navDirections
+import com.example.base_feature.viewState
+import com.example.domain.entities.Show
 import com.example.feature_search.navigation.SearchNavigation
 import com.example.feature_search.presentation.SearchViewModel
 import feature_search.databinding.FragmentSearchBinding
@@ -23,7 +29,8 @@ class SearchFragment : Fragment() {
     private lateinit var adapter: ShowAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
@@ -33,9 +40,17 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        searchViewModel.getSearchShows(binding.searchShow.text.toString())
+        binding.searchShow.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                searchViewModel.search(s.toString())
+
+            }
+        })
 
         setObservers()
+        searchViewModel.getSearchShows()
     }
 
     private fun setObservers() {
@@ -43,6 +58,15 @@ class SearchFragment : Fragment() {
             when (it.state) {
 
                 ViewState.State.SUCCESS -> onSuccess(it.data ?: listOf<ShowPresentation>())
+                ViewState.State.ERROR -> onResultError(it.error)
+                ViewState.State.LOADING -> onLoading(it.isLoading)
+            }
+        })
+
+        searchViewModel.searchShow.observe(viewLifecycleOwner, {
+            when (it.state) {
+
+                ViewState.State.SUCCESS -> onSuccessSearch(it.data ?: listOf<Show>())
                 ViewState.State.ERROR -> onResultError(it.error)
                 ViewState.State.LOADING -> onLoading(it.isLoading)
             }
@@ -67,6 +91,16 @@ class SearchFragment : Fragment() {
         adapter = ShowAdapter(callback = ::clickItem)
         adapter.setItems(list)
         binding.recyclerViewSearch.adapter = adapter
+    }
+
+    private fun onSuccessSearch(list: List<Show>) {
+        onLoading(false)
+        adapter = ShowAdapter(callback = ::clickItem)
+        adapter.setItems(list.toShowModel())
+        binding.recyclerViewSearch.adapter = adapter
+        binding.recyclerViewSearch.visibility = View.VISIBLE
+        binding.cardSearch.visibility = View.GONE
+
     }
 
     private fun clickItem(show: ShowPresentation) {
